5e6e18132ef5881780b8002c480c0e6b72215976
Mark Francis <markxf@gmail.com>
2019-09-24 18:04:43 +0100

    build react front end

---
+++ src/components/app.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifID: "xT9IgDEI1iZyb2wqo8"
+    };
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifID: id
+    });
+  }
+
+  search = (query) => {
+    giphy('ePXdoyI9WLnUtkZZ2jvWqrVD1Py76Gja').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifID} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
